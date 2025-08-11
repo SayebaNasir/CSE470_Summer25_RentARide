66ef4e28fd8a126f0a697885f38be6b59e6b40a8
SayebaNasir <saybanasir74@gmail.com>
2025-08-11 10:08:07 +0600

    new

--- src/pages/MyBookings.jsx
+++ src/pages/MyBookings.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { dummyMyBookingsData } from '../assets/assets'
+import Title from '../components/Title'
 
 const MyBookings = () => {
+  const [bookings, setBookings] = useState([])
+  const fetchMyBookings = async () => {
+    setBookings(dummyMyBookingsData)
+  }
+  useEffect(() => {
+    fetchMyBookings()
+  }, [])
+
   return (
-    <div>
-      
+    <div className='px-6 md:px-16 lg:px-24 xl:px-32 2xl:px-48 mt-16 text-sm max-w-8xl'>
+      <Title 
+        title='My Bookings'
+        subtitle='View and Manage Your All Car Bookings Here!'
+        align="center"
+      />
     </div>
   )
 }
 
 export default MyBookings
+
