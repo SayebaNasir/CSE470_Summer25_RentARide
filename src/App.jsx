61f40d6f35e049cb2fa0f61828a7af83aa6ca71e
SayebaNasir <saybanasir74@gmail.com>
2025-08-20 21:18:24 +0600

    new

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,12 @@ import Home from './pages/Home'
 import CarDetails from './pages/CarDetails'
 import Cars from './pages/Cars'
 import MyBookings from './pages/MyBookings'
+import Layout from './pages/owner/Layout'
+import Dashboard from './pages/owner/Dashboard'
+import AddCar from './pages/owner/AddCar'
+import ManageCars from './pages/owner/ManageCars'
+import ManageBookings from './pages/owner/ManageBookings'
+import Footer from './components/Footer'
 
 const App = () => {
   const [showLogin, setShowLogin] = useState(false)
@@ -22,8 +28,16 @@ const App = () => {
       <Route path='/car-details/:id' element={<CarDetails/>}/>
       <Route path='/cars' element={<Cars/>}/>
       <Route path='/my-bookings' element={<MyBookings/>}/>
-    </Routes>
 
+      <Route path='/owner'element={<Layout/>}>
+        <Route index element={< Dashboard/>}/>
+        <Route path="add-car" element={<AddCar/>}/>
+        <Route path="manage-cars" element={<ManageCars/>}/>
+        <Route path="manage-bookings" element={<ManageBookings/>}/>
+      </Route>
+    </Routes>
+    
+    {!isOwnerpath && <Footer/>}
     </>
   )
 }
